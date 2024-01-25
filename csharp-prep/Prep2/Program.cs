b6e9de9c0c496f7bb30490a61672cf56e91a1474
JackHammond47 <jackson.hammond47@gmail.com>
2024-01-25 16:11:37 -0700

    C# Prep1&2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade Percentage");
+        string gradeFloat = Console.ReadLine();
+        int gradeInt = int.Parse(gradeFloat);
+        string grade = "F";
+        string pass = "N";
+        if (gradeInt >= 90)
+        {
+            grade = "A";
+            pass = "Y";
+        }
+        else if (gradeInt >= 80)
+        {
+            grade = "B";
+            pass = "Y";
+        }
+        else if (gradeInt >= 70)
+        {
+            grade = "C";
+            pass = "Y";
+        }
+        else if (gradeInt >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+        Console.WriteLine(grade);
+        if (pass == "Y")
+        {
+            Console.WriteLine("Congrats! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Oh no, You've failed big time buddy!");
+        }
     }
 }
